6225e0926fe2119c8506830266bf16960cb6f48e
Matheus Pimenta <matheuscscp@gmail.com>
2022-09-17 23:18:56 +0100

    minor refactors

--- hostnetwork/transport_layer.go
+++ hostnetwork/transport_layer.go
@@ -8,24 +8,32 @@ import (
 )
 
 type (
-	transportLayer struct{}
+	transportLayer struct {
+		closed bool
+	}
 )
 
 // NewTransportLayer returns an implementation of transport.Layer backed
 // by Go's standard library "net" (hence on the host network).
 func NewTransportLayer() transport.Layer {
-	return transportLayer{}
+	return &transportLayer{}
 }
 
-func (transportLayer) Listen(ctx context.Context, network, address string) (net.Listener, error) {
-	var lc net.ListenConfig
-	return lc.Listen(ctx, network, address)
+func (t *transportLayer) Listen(ctx context.Context, network, address string) (net.Listener, error) {
+	if t.closed {
+		return nil, transport.ErrProtocolClosed
+	}
+	return (&net.ListenConfig{}).Listen(ctx, network, address)
 }
 
-func (transportLayer) Dial(ctx context.Context, network, address string) (net.Conn, error) {
+func (t *transportLayer) Dial(ctx context.Context, network, address string) (net.Conn, error) {
+	if t.closed {
+		return nil, transport.ErrProtocolClosed
+	}
 	return (&net.Dialer{}).DialContext(ctx, network, address)
 }
 
-func (transportLayer) Close() error {
+func (t *transportLayer) Close() error {
+	t.closed = true
 	return nil
 }
