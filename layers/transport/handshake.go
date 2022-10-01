4c4c50ce6b7a8ba9bfcd6d7f59f4a9f9b443a5c6
rodopoulos <felipekss@gmail.com>
2022-10-01 12:16:41 -0300

    improve variable naming

--- layers/transport/handshake.go
+++ layers/transport/handshake.go
@@ -33,26 +33,26 @@ func (tcp) newClientHandshake() handshake {
 	return &tcpClientHandshake{make(chan tcpSynAck, 1)}
 }
 
-func (h *tcpClientHandshake) recv(segment gopacket.TransportLayer) {
+func (t *tcpClientHandshake) recv(segment gopacket.TransportLayer) {
 	s := segment.(*gplayers.TCP)
 	switch {
 	case s.SYN && s.ACK:
 		select {
-		case h.synack <- tcpSynAck{seq: s.Seq, ack: s.Ack}:
+		case t.synack <- tcpSynAck{seq: s.Seq, ack: s.Ack}:
 		default:
 		}
 	}
 }
 
-func (h *tcpClientHandshake) do(ctx context.Context, c conn) error {
-	t := c.(*tcpConn)
+func (t *tcpClientHandshake) do(ctx context.Context, c conn) error {
+	tc := c.(*tcpConn)
 
 	// send SYN segment
 	seq := rand.Uint32()
-	datagramHeader, segment := t.newDatagramHeaderAndSegment()
+	datagramHeader, segment := tc.newDatagramHeaderAndSegment()
 	segment.SYN = true
 	segment.Seq = seq
-	if err := t.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
+	if err := tc.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
 		return fmt.Errorf("error sending tcp syn segment: %w", err)
 	}
 
@@ -61,7 +61,7 @@ func (h *tcpClientHandshake) do(ctx context.Context, c conn) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case synack := <-h.synack:
+	case synack := <-t.synack:
 		if synack.ack != seq+1 {
 			return fmt.Errorf("peer sent wrong ack number on handshake. want %d, got %d", seq+1, synack.ack)
 		}
@@ -70,15 +70,15 @@ func (h *tcpClientHandshake) do(ctx context.Context, c conn) error {
 	}
 
 	// send ACK segment
-	datagramHeader, segment = t.newDatagramHeaderAndSegment()
+	datagramHeader, segment = tc.newDatagramHeaderAndSegment()
 	segment.ACK = true
 	segment.Ack = ack
-	if err := t.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
+	if err := tc.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
 		return fmt.Errorf("error sending tcp ack segment: %w", err)
 	}
 
-	t.seq = seq
-	t.ack = ack
+	tc.seq = seq
+	tc.ack = ack
 
 	return nil
 }
@@ -87,42 +87,42 @@ func (tcp) newServerHandshake() handshake {
 	return &tcpServerHandshake{make(chan uint32, 1), make(chan uint32, 1)}
 }
 
-func (h *tcpServerHandshake) recv(segment gopacket.TransportLayer) {
+func (t *tcpServerHandshake) recv(segment gopacket.TransportLayer) {
 	s := segment.(*gplayers.TCP)
 	switch {
 	case s.SYN && !s.ACK:
 		select {
-		case h.syn <- s.Seq:
+		case t.syn <- s.Seq:
 		default:
 		}
 	case s.ACK && !s.SYN:
 		select {
-		case h.ack <- s.Ack:
+		case t.ack <- s.Ack:
 		default:
 		}
 	}
 }
 
-func (h *tcpServerHandshake) do(ctx context.Context, c conn) error {
-	t := c.(*tcpConn)
+func (t *tcpServerHandshake) do(ctx context.Context, c conn) error {
+	tc := c.(*tcpConn)
 
 	// receive SYN segment
 	var ack uint32
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case seq := <-h.syn:
+	case seq := <-t.syn:
 		ack = seq + 1
 	}
 
 	// send SYNACK segment
 	seq := rand.Uint32()
-	datagramHeader, segment := t.newDatagramHeaderAndSegment()
+	datagramHeader, segment := tc.newDatagramHeaderAndSegment()
 	segment.SYN = true
 	segment.Seq = seq
 	segment.ACK = true
 	segment.Ack = ack
-	if err := t.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
+	if err := tc.l.s.transportLayer.send(ctx, datagramHeader, segment); err != nil {
 		return fmt.Errorf("error sending tcp synack segment: %w", err)
 	}
 
@@ -130,15 +130,15 @@ func (h *tcpServerHandshake) do(ctx context.Context, c conn) error {
 	select {
 	case <-ctx.Done():
 		return ctx.Err()
-	case ack := <-h.ack:
+	case ack := <-t.ack:
 		if ack != seq+1 {
 			return fmt.Errorf("peer sent wrong ack number on handshake. want %d, got %d", seq+1, ack)
 		}
 		seq++
 	}
 
-	t.seq = seq
-	t.ack = ack
+	tc.seq = seq
+	tc.ack = ack
 
 	return nil
 }
