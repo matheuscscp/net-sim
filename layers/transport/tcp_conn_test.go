8c6b2f0453b70221aaeb1cdb7aab4ad169c7a472
Matheus Pimenta <matheuscscp@gmail.com>
2023-01-08 19:39:54 +0000

    add some tcp tests

--- layers/transport/tcp_conn_test.go
+++ layers/transport/tcp_conn_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"sync"
 	"testing"
@@ -15,8 +16,11 @@ import (
 	"github.com/matheuscscp/net-sim/layers/transport"
 	"github.com/matheuscscp/net-sim/test"
 
+	petname "github.com/dustinkirkland/golang-petname"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func TestTCPConn(t *testing.T) {
@@ -48,17 +52,18 @@ func TestTCPConn(t *testing.T) {
 	require.NotNil(t, networkLayer)
 	transportLayer = transport.NewLayer(networkLayer)
 
-	// start server
+	// start http server
 	serverListener, err := transportLayer.Listen(ctx, transport.TCP, ":80")
 	require.NoError(t, err)
 	require.NotNil(t, serverListener)
 	server = &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			msg := "working"
+		Handler: h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(time.Duration(rand.Intn(5)) * 10 * time.Millisecond)
+			msg := fmt.Sprintf("working: %s", r.URL.Query().Get("q"))
 			n, err := w.Write([]byte(msg))
 			assert.Equal(t, len(msg), n)
 			assert.NoError(t, err)
-		}),
+		}), &http2.Server{}),
 	}
 	wg.Add(1)
 	go func() {
@@ -69,20 +74,60 @@ func TestTCPConn(t *testing.T) {
 		}
 	}()
 
-	// make multiple requests, each one with a new connection
+	// make multiple http1 requests in parallel, each one with a new client (hence new connection)
+	var wgReqs sync.WaitGroup
+	defer wgReqs.Wait()
 	for i := 0; i < 10; i++ {
-		tName := fmt.Sprintf("request%d", i)
-		t.Run(tName, func(t *testing.T) {
-			client := &http.Client{
-				Transport: application.NewHTTPRoundTripper(transportLayer),
-			}
-			resp, err := client.Get("http://127.0.0.1:80/")
+		wgReqs.Add(1)
+		go func() {
+			defer wgReqs.Done()
+			client := &http.Client{Transport: application.NewHTTPRoundTripper(transportLayer)}
+			pet := petname.Generate(2, "_")
+			url := fmt.Sprintf("http://127.0.0.1:80?q=%s", pet)
+			resp, err := client.Get(url)
 			require.NoError(t, err)
 			require.NotNil(t, resp)
 			b, err := io.ReadAll(resp.Body)
 			require.NoError(t, err)
-			assert.Equal(t, []byte("working"), b)
-		})
+			msg := fmt.Sprintf("working: %s", pet)
+			assert.Equal(t, []byte(msg), b)
+		}()
+	}
+
+	// make multiple http1 requests in parallel with the same client (possibly the same connection)
+	client := &http.Client{Transport: application.NewHTTPRoundTripper(transportLayer)}
+	for i := 0; i < 10; i++ {
+		wgReqs.Add(1)
+		go func() {
+			defer wgReqs.Done()
+			pet := petname.Generate(2, "_")
+			url := fmt.Sprintf("http://127.0.0.1:80?q=%s", pet)
+			resp, err := client.Get(url)
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+			b, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			msg := fmt.Sprintf("working: %s", pet)
+			assert.Equal(t, []byte(msg), b)
+		}()
+	}
+
+	// make multiple http2 requests in parallel with the same client (possibly the same connection)
+	client2 := &http.Client{Transport: application.NewH2CRoundTripper(transportLayer)}
+	for i := 0; i < 10; i++ {
+		wgReqs.Add(1)
+		go func() {
+			defer wgReqs.Done()
+			pet := petname.Generate(2, "_")
+			url := fmt.Sprintf("http://127.0.0.1:80?q=%s", pet)
+			resp, err := client2.Get(url)
+			require.NoError(t, err)
+			require.NotNil(t, resp)
+			b, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			msg := fmt.Sprintf("working: %s", pet)
+			assert.Equal(t, []byte(msg), b)
+		}()
 	}
 }
 
